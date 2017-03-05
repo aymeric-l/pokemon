a89d855f84fcbf2b34bbace4d473f677c150a23e
aymeric-l <aymeric-lacoste@hotmail.fr>
2017-03-06 00:18:11 +0100

    meilleur deplacement + debut collision

--- js/script.js
+++ js/script.js
@@ -1,25 +1,81 @@
-var gauche = -790;
-var haut = -770;
+var gauche = -665;
+var haut = -790;
 var keys = {};
+var deplacement = true;
 
-$(window).on("keyup keydown", function (e) {
-    e = e || event;
-    keys[e.keyCode] = e.type === 'keydown';
 
-    if (keys[90]) {
-        haut = haut+80;
-	$("#img").animate({top: (+haut+('px'))}, "fast");
-    }
-    if (keys[83]) {
-        haut = haut-80;
-	$("#img").animate({top: (+haut+('px'))}, "fast");
-    }
-    if (keys[81]) {
-        gauche = gauche+80;
-	$("#img").animate({left: (+gauche+('px'))}, "fast");
-    }
-    if (keys[68]) {
-        gauche = gauche-80;
-	$("#img").animate({left: (+gauche+('px'))}, "fast");
-    }
+window.addEventListener("keydown", function dep (event) {
+
+////// BAS ///////
+  if (event.keyCode == 83) {
+    if(deplacement == true){
+    haut = haut-80;
+    console.log('yy');
+    $("#img").animate({top: (+haut+('px'))}, 300);
+    deplacement = false;
+
+    setTimeout(function(){
+    deplacement = true;
+    },300);
+}
+}
+
+////// HAUT ///////
+if (event.keyCode == 90) {
+    if(deplacement == true){
+        /// COLLISION ///
+        if(gauche == -1305 && haut == -710){}
+        else if(gauche == -1385 && haut == -710){}//////
+        else if(gauche == -1465 && haut == -710){}//
+        else if(gauche == -1545 && haut == -710){}// GRAND BATIMENT ROUGE
+        else if(gauche == -1705 && haut == -710){}// 
+        else if(gauche == -1785 && haut == -710){}//////
+        else if(gauche == -1865 && haut == -710){}// CARRE DROIT BATIMENT ROUGE
+        else if(gauche == -1945 && haut == -630){}/////
+        else{
+    haut = haut+80;
+    $("#img").animate({top: (+haut+('px'))}, 300);
+    deplacement = false;
+
+    setTimeout(function(){
+       deplacement = true;
+    },300);
+}
+}
+}
+
+////// GAUCHE ///////
+if (event.keyCode == 81) {
+    if(deplacement == true){
+        /// COLLISION ///
+        if(gauche == -1945 && haut == -630){}
+        else{
+    gauche = gauche+80;
+    $("#img").animate({left: (+gauche+('px'))}, 300);
+    deplacement = false;
+
+    setTimeout(function(){
+       deplacement = true;
+    },300);
+}
+}
+}
+
+////// DROITE //////
+if (event.keyCode == 68) {
+    if(deplacement == true){
+        /// COLLISION ///
+        if(gauche == -1225 && haut == -630){}
+        else if(gauche == -1945 && haut == -630){}
+        else{
+    gauche = gauche-80;
+    $("#img").animate({left: (+gauche+('px'))}, 300);
+    deplacement = false;
+
+    setTimeout(function(){
+       deplacement = true;
+    },300);
+}
+}
+}
 });
