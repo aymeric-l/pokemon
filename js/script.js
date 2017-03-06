4b49fc30e76a490a6fdb8546341049ee71f197a3
aymeric-l <aymeric-lacoste@hotmail.fr>
2017-03-06 14:27:57 +0100

    Optimisation : foutage de gueule

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-var gauche = -665;
-var haut = -790;
+var gauche = -825;
+var haut = -870;
+var MaisonTop = 420;
+var MaisonLeft = 650;
 var keys = {};
 var deplacement = true;
 
@@ -9,14 +11,40 @@ window.addEventListener("keydown", function dep (event) {
 ////// BAS ///////
   if (event.keyCode == 83) {
     if(deplacement == true){
+        if(gauche ==  -1305 && haut == -550){}
+        else if(gauche ==  -2345 && haut == -870){}
+        else if(gauche ==  -2265 && haut == -870){}
+        else if(gauche ==  -2185 && haut == -870){}
+        else if(gauche ==  -2105 && haut == -870){} 
+        else if(gauche ==  -2025 && haut == -870){}
+        else if(gauche ==  -1945 && haut == -870){}
+        else if(gauche ==  -1865 && haut == -1030){}
+        else if(gauche ==  -1785 && haut == -1030){}
+        else if(gauche ==  -1705 && haut == -1030){}
+        else if(gauche ==  -1625 && haut == -1030){}
+        else if(gauche ==  -1545 && haut == -1030){}
+        else if(gauche ==  -1465 && haut == -1030){}
+        else if(gauche ==  -1385 && haut == -1030){}
+        else if(gauche ==  -1305 && haut == -1110){}
+        else if(gauche ==  -1225 && haut == -1110){}
+        else if(gauche ==  -1145 && haut == -1030){}
+        else if(gauche ==  -1065 && haut == -950){}
+        else if(gauche ==  -985 && haut == -950){}
+        else if(gauche ==  -905 && haut == -950){}
+        else if(gauche ==  -825 && haut == -950){}
+
+        else{
     haut = haut-80;
+    MaisonTop = MaisonTop-80;
     console.log('yy');
     $("#img").animate({top: (+haut+('px'))}, 300);
+    $("#maison").animate({top: (+MaisonTop+('px'))}, 300);
     deplacement = false;
 
     setTimeout(function(){
     deplacement = true;
-    },300);
+    },301);
+}
 }
 }
 
@@ -32,9 +60,28 @@ if (event.keyCode == 90) {
         else if(gauche == -1785 && haut == -710){}//////
         else if(gauche == -1865 && haut == -710){}// CARRE DROIT BATIMENT ROUGE
         else if(gauche == -1945 && haut == -630){}/////
+        else if(gauche ==  -1305 && haut == -390){}
+        else if(gauche ==  -1225 && haut == -390){}
+        else if(gauche ==  -1145 && haut == -390){}
+        else if(gauche ==  -1065 && haut == -390){}
+        else if(gauche ==  -2025 && haut == -710){}
+        else if(gauche ==  -2105 && haut == -710){}
+        else if(gauche ==  -2185 && haut == -710){}
+        else if(gauche ==  -2265 && haut == -710){}
+        else if(gauche ==  -2345 && haut == -710){}
+        else if(gauche ==  -825 && haut == -1270){}
+        else if(gauche ==  -905 && haut == -1270){}
+        else if(gauche ==  -985 && haut == -1270){}
+        else if(gauche ==  -1065 && haut == -1270){}
+        else if(gauche ==  -1145 && haut == -1830){}
+        else if(gauche ==  -1225 && haut == -1830){}
+        else if(gauche ==  -1305 && haut == -1830){}
+        else if(gauche ==  -1385 && haut == -1750){}
         else{
     haut = haut+80;
+    MaisonTop = MaisonTop+80;
     $("#img").animate({top: (+haut+('px'))}, 300);
+    $("#maison").animate({top: (+MaisonTop+('px'))}, 300);
     deplacement = false;
 
     setTimeout(function(){
@@ -49,9 +96,15 @@ if (event.keyCode == 81) {
     if(deplacement == true){
         /// COLLISION ///
         if(gauche == -1945 && haut == -630){}
+        else if(gauche ==  -1065 && haut == -390){}
+        else if(gauche ==  -1225 && haut == -1110){}
+        else if(gauche ==  -1145 && haut == -1030){}
+        else if(gauche ==  -1385 && haut == -1750){}
         else{
     gauche = gauche+80;
+    MaisonLeft = MaisonLeft+80;
     $("#img").animate({left: (+gauche+('px'))}, 300);
+    $("#maison").animate({left: (+MaisonLeft+('px'))}, 300);
     deplacement = false;
 
     setTimeout(function(){
@@ -67,9 +120,31 @@ if (event.keyCode == 68) {
         /// COLLISION ///
         if(gauche == -1225 && haut == -630){}
         else if(gauche == -1945 && haut == -630){}
+        else if(gauche ==  -1305 && haut == -550){}
+        else if(gauche ==  -1305 && haut == -470){}
+        else if(gauche ==  -1305 && haut == -390){}
+        else if(gauche ==  -2345 && haut == -710){}
+        else if(gauche ==  -2345 && haut == -790){}
+        else if(gauche ==  -2345 && haut == -870){}
+        else if(gauche ==  -1865 && haut == -950){}
+        else if(gauche ==  -1865 && haut == -1030){}
+        else if(gauche ==  -1305 && haut == -1110){}
+        else if(gauche ==  -745 && haut == -1030){}
+        else if(gauche ==  -745 && haut == -1110){}
+        else if(gauche ==  -745 && haut == -1190){}
+        else if(gauche ==  -1065 && haut == -1270){}
+        else if(gauche ==  -1065 && haut == -1350){}
+        else if(gauche ==  -1065 && haut == -1430){}
+        else if(gauche ==  -1065 && haut == -1510){}
+        else if(gauche ==  -1065 && haut == -1590){}
+        else if(gauche ==  -1065 && haut == -1670){}
+        else if(gauche ==  -1065 && haut == -1750){}
+        else if(gauche ==  -1385 && haut == -1750){}
         else{
     gauche = gauche-80;
+    MaisonLeft = MaisonLeft-80;
     $("#img").animate({left: (+gauche+('px'))}, 300);
+    $("#maison").animate({left: (+MaisonLeft+('px'))}, 300);
     deplacement = false;
 
     setTimeout(function(){
