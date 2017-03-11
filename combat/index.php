c3cef3eb1e55c65770886eb477a0387e5e0cfb3d
Lacoste Aymeric <aymeric-lacoste@hotmail.fr>
2017-03-11 15:48:35 +0100

    combat

---
+++ combat/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Combat pokemon</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css"></link>
+</head>
+<body>
+<div id="contenaire">
+<div id="un">
+	<div id="unUn">
+	<div id="pousser"></div>
+		<div id="infoEnnemi">
+			<div id="nom">CARAPUCE</div>
+			<div id="niveau">Lvl : 5</div>
+			<div id="vie">PV : 100</div>
+		</div>
+		<div id="ennemi"><img src="pokemon/carapuce.png"></div>
+	</div>
+	<div id="unDeux">
+		<div id="pokemon"><img src="pokemon/salameche.png"></div>
+		<div id="info">
+			<div id="nom">SALAMECHE</div>
+			<div id="niveau">Lvl : 6</div>
+			<div id="vie">PV : 90</div>
+		</div>
+	</div>
+</div>
+
+
+
+<div id="deux">
+	<div id="textChoix">
+		<p>Que doit faire SALAMECHE ?</p>
+	</div><div id="choix">
+		<div id="choixUn"><p>ATTAQUE</p></div>
+	</div>
+</div>
+
+</div>
+</body>
+</html>
